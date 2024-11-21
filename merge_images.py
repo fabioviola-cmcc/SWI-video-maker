1c98c3e7f9c9f20e061d8a7404c7702ad6c593e7
Fabio Viola <fabio.viola@cmcc.it>
2024-11-21 16:58:04 +0100

    Added overlay of logo

--- merge_images.py
+++ merge_images.py
@@ -22,13 +22,14 @@ import os
 # input params
 # - the folder containing the images to be merged
 # - 0 or 1 to choose between horiz. or vertical merge
+# - the image to overlay
 #
 ######################################################
 
 # Directory where your images are located
 image_dir = sys.argv[1]
 vertical = int(sys.argv[2]) # 0 = horizontal, 1 = vertical
-
+overlay = sys.argv[3] 
 
 ######################################################
 #
@@ -50,11 +51,11 @@ for d in range(0, 366):
         continue
     
     # salinity
-    salinity_img = Image.open(salinity_plot)
+    salinity_img = Image.open(salinity_plot).convert("RGBA")
     salinity_width, salinity_height = salinity_img.size
     
     # lx
-    lx_img = Image.open(lx_plot)
+    lx_img = Image.open(lx_plot).convert("RGBA")
     lx_width, lx_height = lx_img.size
 
     if vertical:
@@ -73,6 +74,10 @@ for d in range(0, 366):
         # combined_img.paste(lx_img_res, (salinity_width, 0))
         combined_img.paste(lx_img, (0, salinity_height))
     
+        # overlay the logo        
+        logo_img = Image.open(overlay).convert("RGBA")
+        combined_img.paste(logo_img, (0,0), logo_img)
+
         # Save the combined image with a new filename
         print(f'Saving VERT_combined_plot_{d:04d}.png')
         combined_img.save(os.path.join(image_dir, f'VERT_combined_plot_{d:04d}.png'))
@@ -90,13 +95,18 @@ for d in range(0, 366):
         combined_img.paste(salinity_img, (0, 0))
     
         # Paste the lx image on the right
-        # combined_img.paste(lx_img_res, (salinity_width, 0))
         combined_img.paste(lx_img, (salinity_width, 0))
+
+        # overlay the logo
+        logo_img = Image.open(overlay).convert("RGBA")
+        combined_img.paste(logo_img, (0,0), logo_img)
     
         # Save the combined image with a new filename
         print(f'Saving HORIZ_combined_plot_{d:04d}.png')
         combined_img.save(os.path.join(image_dir, f'HORIZ_combined_plot_{d:04d}.png'))
-    
+
+    break
+        
     # Close the images to free up resources
     salinity_img.close()
     lx_img.close()
