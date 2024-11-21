76bfd8879d6d780f2a00a2a2f160c6e5b611d18a
Fabio Viola <fabio.viola@cmcc.it>
2024-11-21 15:04:04 +0100

    Added first script to merge images

---
+++ merge_images.py
@@ -0,0 +1,94 @@
+######################################################
+#
+# requirements
+#
+######################################################
+
+from PIL import Image
+import sys
+import os
+
+
+######################################################
+#
+# input params
+# - the folder containing the images to be merged
+# - 0 or 1 to choose between horiz. or vertical merge
+#
+######################################################
+
+# Directory where your images are located
+image_dir = sys.argv[1]
+vertical = int(sys.argv[2]) # 0 = horizontal, 1 = vertical
+
+
+######################################################
+#
+# process images
+#
+######################################################
+
+# iterate over the dates to 
+for d in range(0, 366):
+
+    # build filenames
+    salinity_plot = os.path.join(image_dir, f"20241010_salinity_plot_{d:04d}.png")
+    lx_plot = os.path.join(image_dir, f"20241010_lx_plot_{d:04d}.png")
+
+    # check if files exist, otherwise skip to next iteration
+    if not os.path.exists(salinity_plot):
+        continue
+    if not os.path.exists(lx_plot):
+        continue
+    
+    # salinity
+    salinity_img = Image.open(salinity_plot)
+    salinity_width, salinity_height = salinity_img.size
+    
+    # lx
+    lx_img = Image.open(lx_plot)
+    lx_width, lx_height = lx_img.size
+
+    if vertical:
+
+        # Calculate the total width and height for the combined image
+        total_width = max(salinity_width, lx_width)
+        total_height = salinity_height + lx_height
+    
+        # Create a new blank image with the calculated size
+        combined_img = Image.new('RGB', (total_width, total_height), color='white')
+    
+        # Paste the salinity image on the left
+        combined_img.paste(salinity_img, (0, 0))
+    
+        # Paste the lx image on the right
+        # combined_img.paste(lx_img_res, (salinity_width, 0))
+        combined_img.paste(lx_img, (0, salinity_height))
+    
+        # Save the combined image with a new filename
+        print(f'Saving VERT_combined_plot_{d:04d}.png')
+        combined_img.save(os.path.join(image_dir, f'VERT_combined_plot_{d:04d}.png'))
+
+    else:
+        
+        # Calculate the total width and height for the combined image
+        total_width = salinity_width + lx_width
+        total_height = max(salinity_height, lx_height)
+    
+        # Create a new blank image with the calculated size
+        combined_img = Image.new('RGB', (total_width, total_height), color='white')
+    
+        # Paste the salinity image on the left
+        combined_img.paste(salinity_img, (0, 0))
+    
+        # Paste the lx image on the right
+        # combined_img.paste(lx_img_res, (salinity_width, 0))
+        combined_img.paste(lx_img, (salinity_width, 0))
+    
+        # Save the combined image with a new filename
+        print(f'Saving HORIZ_combined_plot_{d:04d}.png')
+        combined_img.save(os.path.join(image_dir, f'HORIZ_combined_plot_{d:04d}.png'))
+    
+    # Close the images to free up resources
+    salinity_img.close()
+    lx_img.close()
